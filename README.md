3fecf2d344afd1d48dff688b2693f687e115bd2f
swagat030 <swagatkumarmishra888@gmail.com>
2023-11-14 16:57:56 +0530

    React app updated

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sahosoft-react-app
+
+React Practise App
+
+#Parcel
+
+1. Dev Build
+2. Local Server
+3. HMR = Hot Module Replacement
+4. File Watching Algorithm: written in C++
+5. Caching - Faster Builds
+6. Image Optimization
+7. Minification (Productions Builds)
+8. Bundling
+9. Compressing
--- dist/index.6bd02f5a.js
+++ dist/index.6bd02f5a.js
@@ -1,14 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 /**
 const heading = React.createElement("h1", {id: 'myHeading'}, "Hello World from React");
 console.log(heading);
 const parentData = document.getElementById('root');
 const root = ReactDOM.createRoot(parentData);
 root.render(heading);
-*/
-
-/*const heading = React.createElement('div', {id: "parent"}, [
+*/ /*const heading = React.createElement('div', {id: "parent"}, [
     React.createElement('div', {id: 'child1'}, [
         React.createElement('h1', {id: "h1TagChild1"}, "I am from h1 Tag Child1"),
         React.createElement('h2', {id: "h2TagChild1"}, "I am from h2 Tag Child1"),
@@ -17,10 +13,12 @@ root.render(heading);
         React.createElement('h1', {id: "h1TagChild2"}, "I am from h1 Tag Child2"),
         React.createElement('h2', {id: "h2TagChild2"}, "I am from h2 Tag Child2"),
     ])
-]);*/
-
-const heading = React.createElement("h1", {id: 'parent'}, "Hello World from React");
+]);*/ const heading = React.createElement("h1", {
+    id: "parent"
+}, "Hello World from React");
 console.log(heading);
 const parentData = document.getElementById("root");
 const root = ReactDOM.createRoot(parentData);
 root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
