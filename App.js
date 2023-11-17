c385433b8fdb609cf083a9feb9a4cea9b414454f
swagat030 <swagatkumarmishra888@gmail.com>
2023-11-18 01:08:18 +0530

    File updated

--- App.js
+++ App.js
@@ -5,13 +5,43 @@ import ReactDOM from "react-dom/client";
 // const heading =
 //     React.createElement("h1", {id: "parent"}, "Hello World from React 🚀");
 // console.log(heading);
-const jsxHeading =
+// const jsxHeading =
+//     (<div id = "heading">
+//     <h1 id="parent" className="head">
+//     Hello World from React🚀</h1>
+//     </div>);
+//     // JSX Code => React Element => JavaScript Object => renders to actual DOM as a HTML Element
+// console.log(jsxHeading);
+// const parentData = document.getElementById("root");
+// const root = ReactDOM.createRoot(parentData);
+// root.render(jsxHeading);
+
+// const HeadingComponent = () => {
+//     return <h1 id="parent">My Functional based Component</h1>
+// };
+
+const element = <span>React Element</span>;
+
+
+const number = 1000;
+
+const HeadingComponent = () => (
+    <div id="parent">
+        <h1>My Functional based Component🚀</h1> 
+    </div>
+);
+
+const headingElement =
     (<div id = "heading">
+        {number}
     <h1 id="parent" className="head">
     Hello World from React🚀</h1>
-    </div>);
-    // JSX Code => React Element => JavaScript Object => renders to actual DOM as a HTML Element
-console.log(jsxHeading);
-const parentData = document.getElementById("root");
-const root = ReactDOM.createRoot(parentData);
-root.render(jsxHeading);
+    <HeadingComponent/>
+    </div>); 
+
+console.log(HeadingComponent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(headingElement);
+
