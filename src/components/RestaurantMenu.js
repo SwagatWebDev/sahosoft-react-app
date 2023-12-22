82d17ba3ff75d9219d56a3e54f1a27e4bd4eef99
swagat030 <swagatkumarmishra888@gmail.com>
2023-12-22 18:58:55 +0530

    File updated

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,13 +1,12 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import Shimmer from "./Shimmer";
 import RestaurantCategory from "./RestaurantCategory";
-import {useState} from "react";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
     const { resId } = useParams();
     const resInfo = useRestaurantMenu(resId);
-
     const [showIndex, setShowIndex] = useState(null);
 
     if (resInfo === null) return <Shimmer />;
@@ -16,7 +15,7 @@ const RestaurantMenu = () => {
     const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
     const categoryType = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
-        .filter(category => category?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+        .filter(category => category?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
 
     console.log(categoryType);
 
@@ -24,13 +23,20 @@ const RestaurantMenu = () => {
         <div className="menu p-8 text-center">
             <h1 className="text-3xl font-bold mb-4">{name}</h1>
             <p className="text-gray-600">{cuisines.join(", ")} - {costForTwoMessage}</p>
-            {categoryType.map((category, index) => (<RestaurantCategory
-                key={category?.card?.title}
-                data={category?.card?.card}
-                showItems = {index === showIndex ? true : false}
-                setShowIndex = {() => setShowIndex((prevIndex) =>
-                    (prevIndex === null || prevIndex !== index) ? index : null)}
-            />))}
+            {categoryType.length > 0 ? (
+                categoryType.map((category, index) => (
+                    <RestaurantCategory
+                        key={category?.card?.card.title}
+                        data={category?.card?.card}
+                        showItems={index === showIndex}
+                        setShowIndex={() => setShowIndex((prevIndex) => (prevIndex === null || prevIndex !== index) ? index : null)}
+                    />
+                ))
+            ) : (
+                <div className="text-gray-600 font-bold mt-4">
+                    No Menu Found. Check back later or explore other options.
+                </div>
+            )}
         </div>
     );
 };
